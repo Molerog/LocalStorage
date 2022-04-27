c8c05404027647c889110f9ac077bb1bc89c0d78
Molerog <102958319+Molerog@users.noreply.github.com>
2022-04-27 12:42:50 +0200

    Local Storage

---
+++ storage.js
@@ -0,0 +1,10 @@
+const submit = document.getElementById('containerSubmit');
+const nameInfo = document.getElementById('Name');
+const emailInfo = document.getElementById('Email');
+
+console.log(submit,nameInfo,emailInfo)
+
+
+
+
+
